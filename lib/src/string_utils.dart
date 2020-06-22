bea56ec9c49740cf48588da4ac23490b15ae84cd
LinuxServer <Jerome.Wolff1207@gmail.com>
2020-06-22 15:00:28 +0200

    Add more string utils

--- lib/src/string_utils.dart
+++ lib/src/string_utils.dart
@@ -6,4 +6,24 @@ class Strings {
   static bool isBlank(String text) {
     return !isNotBlank(text);
   }
+
+  static bool isEmpty(String text) {
+    return text == null || text.isEmpty;
+  }
+
+  static List<String> split(String text, Pattern pattern,
+      {List<String> defaultValue = const []}) {
+    List<String> list = text?.split(pattern);
+    return list ?? defaultValue;
+  }
+
+  static String reverse(String text) {
+    if (isEmpty(text)) return '';
+    StringBuffer buffer = StringBuffer();
+
+    for (int index = text.length - 1; index >= 0; index--) {
+      buffer.writeCharCode(text.codeUnitAt(index));
+    }
+    return buffer.toString();
+  }
 }
