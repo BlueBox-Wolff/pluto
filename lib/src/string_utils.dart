40b9b99c2964121d23277c19afd1fe0c5792201a
LinuxServer <jerome.wolff1207@gmail.com>
2020-06-28 16:33:54 +0200

    Add many more string utils

--- lib/src/string_utils.dart
+++ lib/src/string_utils.dart
@@ -26,4 +26,48 @@ class Strings {
     }
     return buffer.toString();
   }
+
+  static String removeAlphabet(String text) {
+    var temp = text.replaceAll(RegExp('[^0-9.]'), '');
+    return temp;
+  }
+
+  static bool isAlphabetDigitString(String text) {
+    assert(text != null);
+    return text?.contains(RegExp(r'[^A-Za-z0-9]'));
+  }
+
+  static String trimString(String text) {
+    if (text == null) {
+      return text;
+    }
+
+    String toReturn = text.trim();
+    toReturn = toReturn.replaceAll(RegExp('  +'), ' ');
+    toReturn = toReturn.replaceAll(RegExp('\t\t+'), '\t');
+    toReturn = toReturn.replaceAll(RegExp('\n\n\n+'), '\n\n');
+    return toReturn;
+  }
+
+  static List<String> indexable(String text) {
+    if (text == null) {
+      return [];
+    }
+    return Set.of(text
+        .split(RegExp(r'[^A-Za-z0-9]'))
+        .map((element) => element.toLowerCase())
+        .where((element) => element != '')).toList();
+  }
+
+  static String ellipsis(String toBeShorted, int endIndex) {
+    if (toBeShorted == null) {
+      return '';
+    }
+
+    String temp = toBeShorted;
+    if (temp.length >= endIndex) {
+      temp = temp.substring(0, endIndex - 3) + '...';
+    }
+    return temp;
+  }
 }
